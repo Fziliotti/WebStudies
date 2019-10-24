4290d19bbe16fb71ee3bc692cf2eea60c71ba5c1
Fabrício Ziliotti <ffz.ziliotti@gmail.com>
2019-10-24 02:58:47 -0300

    Adicionando mais estudos

---
+++ Algortimos/Sort_Python/main.py
@@ -0,0 +1,64 @@
+
+def mergeSort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)
+    if(fim - inicio > 1):
+        meio = (fim + inicio)//2
+        mergeSort(lista, inicio, meio)
+        mergeSort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
+
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    top_left, top_right = 0, 0
+    for k in range(inicio, fim):
+        if top_left >= len(left):
+            lista[k] = right[top_right]
+            top_right = top_right + 1
+        elif top_right >= len(right):
+            lista[k] = left[top_left]
+            top_left = top_left + 1
+        elif left[top_left] < right[top_right]:
+            lista[k] = left[top_left]
+            top_left = top_left + 1
+        else:
+            lista[k] = right[top_right]
+            top_right = top_right + 1
+
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(1, n):
+        chave = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > chave:
+            lista[j+1] = lista[j]
+            j = j - 1
+        lista[j+1] = chave
+# Complexidade de tempo O(nˆ2)
+# Complexidade de espaço O(n)
+
+def bubble_sort(lista):
+    n = len(lista)
+    for j in range(n-1):
+        for i in range(n-1):
+            if lista[i] > lista[i+1]:
+                # troca de elementos nas posições i e i+1
+                lista[i], lista[i+1] = lista[i+1], lista[i]
+# Complexidade de tempo O(nˆ2)
+# Complexidade de espaço O(n)
+
+def selection_sort(lista):
+    n = len(lista)
+    for j in range(n-1):
+        min_index = j
+        for i in range(j, n):
+            if lista[i] < lista[min_index]:
+                min_index = i
+        if lista[j] > lista[min_index]:
+            aux = lista[j]
+            lista[j] = lista[min_index]
+            lista[min_index] = aux
+# 1 + (n-1)*[5 + X] = 1 + 5*(n-1) + X*(n-1)
+# Complexidade de tempo O(nˆ2)
+# Complexidade de espaço O(n)
